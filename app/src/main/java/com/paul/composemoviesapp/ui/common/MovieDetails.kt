2abc168d8efd77c8a88ae0ca37040133c1095e47
Tekkiller <paulnjoroge789016@gmail.com>
2021-09-01 10:45:14 +0300

    Creating movie details layout

--- app/src/main/java/com/paul/composemoviesapp/ui/common/MovieDetails.kt
+++ app/src/main/java/com/paul/composemoviesapp/ui/common/MovieDetails.kt
@@ -1,2 +1,69 @@
 package com.paul.composemoviesapp.ui.common
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.paul.composemoviesapp.R
+
+@Preview
+@Composable
+fun MovieDetails() {
+
+}
+
+@Preview
+@Composable
+fun MovieHeader() {
+
+    Column (
+        Modifier
+            .padding(all = 0.dp)
+            .height(200.dp)
+            .fillMaxWidth(),
+
+
+
+
+
+    ) {
+
+
+
+        Card(
+            elevation = 4.dp,
+
+        ){
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+
+            )
+
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+                Image(
+                    painter = painterResource(R.drawable.youtube),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
+                    ,
+
+                    )
+            }
+
+        }
+
+    }
+}
